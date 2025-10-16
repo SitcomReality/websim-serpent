ca1f4d0912ad7d314588c0ff10fcd2b7af322491
SitcomReality <SitcomReality@websim.ai>
2025-10-16 15:50:41 +0000

    v16: Reduce the size of the spark particle (js/effects/SparkParticle.js) from .25 to .05

--- js/effects/SparkParticle.js
+++ js/effects/SparkParticle.js
@@ -1,7 +1,7 @@
 import { Vector2D } from '../utils/Vector2D.js';
 
 export class SparkParticle {
-    constructor(x, y, velocity, color = '#fff', size = 0.1) {
+    constructor(x, y, velocity, color = '#fff', size = 0.05) {
         this.pos = new Vector2D(x, y);
         this.velocity = velocity.copy().mult(1.25);
         this.color = color;
