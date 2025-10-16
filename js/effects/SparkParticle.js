99f408baba2d993d8048385ffbc44c9d7038f1f7
SitcomReality <SitcomReality@websim.ai>
2025-10-16 15:56:00 +0000

    v18: Version 18

--- js/effects/SparkParticle.js
+++ js/effects/SparkParticle.js
@@ -3,7 +3,7 @@ import { Vector2D } from '../utils/Vector2D.js';
 export class SparkParticle {
     constructor(x, y, velocity, color = '#fff', size = 0.1) {
         this.pos = new Vector2D(x, y);
-        this.velocity = velocity.copy().mult(1.25);
+        this.velocity = velocity.copy().mult(1);
         this.color = color;
         this.size = size;
         this.life = (0.5 + Math.random() * 0.6) * 0.75; // shorter life (~25% less)
