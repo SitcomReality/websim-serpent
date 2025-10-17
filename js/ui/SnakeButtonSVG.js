74bbbfb39f81bbee887f672822ad73a2a207d912
SitcomReality <SitcomReality@websim.ai>
2025-10-17 13:30:41 +0000

    v73: We just made js/ui/SnakeButtonSVG.js to use for our buttons, but currently the only part of the buttons that shows is their text and the tongue shows when they're hovered. The sole exception is the quit button on the pause screen. Can you please see

--- js/ui/SnakeButtonSVG.js
+++ js/ui/SnakeButtonSVG.js
@@ -8,7 +8,7 @@ export function snakeButtonSVG(gradientId = 'snakeGradient') {
             </linearGradient>
         </defs>
         <!-- Snake body and tail as one continuous path -->
-        <path class="snake-body" d="M 190, 30 Q 175, 10, 150, 10 L 50, 10 Q 25, 10, 25, 30 Q 25, 50, 50, 50 L 150, 50 Q 175, 50, 190, 30 Z" />
+        <path class="snake-body" d="M 190, 30 Q 175, 10, 150, 10 L 50, 10 Q 25, 10, 25, 30 Q 25, 50, 50, 50 L 150, 50 Q 175, 50, 190, 30 Z" fill="url(#${gradientId})" />
         <!-- Forked tongue -->
         <path class="snake-tongue" d="M 30, 27 L 10, 20 M 30, 33 L 10, 40" stroke="#ff7675" stroke-width="2" stroke-linecap="round" fill="none" />
     </svg>
